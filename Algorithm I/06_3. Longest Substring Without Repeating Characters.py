647eeac58a8cb3da50ed7d668f489a16c64bc9f3
lama_imp <lakeyeva.marina@gmail.com>
2021-08-31 12:32:53 +0400

    Day 6

---
+++ Algorithm I/06_3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,17 @@
+def lengthOfLongestSubstring(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    ans = ''
+    len_ans = 0
+    for i in range(len(s)):
+        ans = s[i]
+        for j in range(i + 1, len(s)):
+            if s[j] not in ans:
+                ans += s[j]
+            else:
+                break
+        if len(ans) > len_ans:
+            len_ans = len(ans)
+    return len_ans
